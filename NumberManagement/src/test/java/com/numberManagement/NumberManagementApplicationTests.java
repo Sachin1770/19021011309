ecae724618c4df3ce9cb50a0807df2619172de37
Sachin Chauhan <94624664+Sachin1770@users.noreply.github.com>
2023-08-23 12:01:32 +0530

    Add files via upload

---
+++ NumberManagement/src/test/java/com/numberManagement/NumberManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.numberManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumberManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
